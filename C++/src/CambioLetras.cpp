03b9db94f6effaafa45707bc4b4b673136ec6e8c
Clon5454 <clon5454@outlook.com>
2023-04-05 12:51:46 -0500

    funcional

--- C++/src/CambioLetras.cpp
+++ C++/src/CambioLetras.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     std::string cadena = "La lluvia en Sevilla es una maravilla";
-    char convert[100];
+    char convert[50];
     strcpy(convert, cadena.c_str());
+
+    cout << convert << "\n";
 
     return 0;
 }
