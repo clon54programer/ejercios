d0db0a039eb55317bb465296219a9c568782b445
Clon5454 <clon5454@outlook.com>
2023-04-05 12:37:33 -0500

    haciendo un jercio en c++

---
+++ C++/src/CambioLetras.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello worls \n";
+    char c = 'K';
+
+    int i = c;
+    cout << i;
+    return 0;
+}
